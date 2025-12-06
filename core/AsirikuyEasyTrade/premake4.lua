8bebc2600defc068eb7560f60e2de0e26a578449
amo31 <amo3167@gmail.com>
2025-12-06 13:33:00 +1100

    Enable Boost support on Windows with stub libraries
    - Restore Precompiled.hpp usage on all platforms (Windows, Linux, Mac)
    - Windows still uses Boost includes (excludes only boost/thread.hpp due to compatibility)
    - Windows uses CRITICAL_SECTION from Windows.h for multi-threading
    - Create stub .lib files for Boost libraries to satisfy linker
    - Boost includes are available on Windows for header-only and asio/filesystem/etc
    
    Configuration:
    - EasyTradeCWrapper.cpp uses Precompiled.hpp on all platforms
    - Precompiled.hpp already conditionally excludes boost/thread on Windows
    - Stub libraries allow compilation without full Boost build
    
    Result: Windows builds can now use Boost includes while maintaining CRITICAL_SECTION support

--- core/AsirikuyEasyTrade/premake4.lua
+++ core/AsirikuyEasyTrade/premake4.lua
@@ -12,12 +12,9 @@ project "AsirikuyEasyTrade"
     "../AsirikuyCommon/include",
     "../../vendor/boost_1_49_0"
   }
-  -- Only link Boost libraries on non-Windows platforms
-  configuration{"not windows"}
-    libdirs{
-      "../../vendor/boost_1_49_0/stage/lib"
-    }
-  
+  libdirs{
+    "../../vendor/boost_1_49_0/stage/lib"
+  }
   -- Add Boost include directory (for Precompiled.hpp)
   configuration{"not windows"}
     includedirs{
